80b26b9fcd4289dde65640aee6fb9446ab76f1b7
Zamoca <suntail2002@naver.com>
2023-10-26 16:17:04 +0900

    Chore: NestJS 앱 모듈에서 MongoDB 설정
    - MongoDB 연결을 위해서 Mongoose를 이용해 앱 모듈에 설정
    
    Related to #1

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { validate } from './common/env.validation';
+import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
@@ -10,6 +11,13 @@ import { validate } from './common/env.validation';
       envFilePath: `.env.${process.env.NODE_ENV}`,
       isGlobal: true,
       validate,
+    }),
+    MongooseModule.forRoot(process.env.DATABASE_URI, {
+      dbName: process.env.DATABASE_NAME,
+      auth: {
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASS,
+      },
     }),
   ],
   controllers: [AppController],
