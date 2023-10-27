1b2dda7f7472d9fbbb4c8c8112da90ce326c475e
myeongseoklee <yhkyhk92@gmail.com>
2023-10-28 00:48:25 +0900

    Feat : 게시물 통계 스키마 구현 #13
    게시물 통계 데이터를 저장할 스키마를 구현하였습니다.
    - 통계 데이터 생성 시 각 필드는 모두 요구되는 값이기 때문에, required true 조건 적용
    - contentId: sns 플랫폼에서 제공하는 게시물의 id 입니다. sns 별로 id 값이 다를 수 있기 때문에 string 타입을 적용했습니다.
    - snsType: sns 서비스의 이름입니다. enum 타입 validation을 적용하였습니다.
    > enum validation option에는 원시 값의 배열, 또는 스키마 생성자 타입의 배열(ex. String, Number 등)만 삽입 가능하므로 즉시 실행함수(ArticleSnsSchemaType)를 사용하여 값을 런타임에 생성하여 적용하도록 구현했습니다.
    - hashtags: 게시물에 포함된 해시태그입니다. string[] 타입 적용했습니다.
    - view, like, share: ~CountByDate 형식(ex. viewCountByDate)으로 네이밍하여 필드가 가진 값을 직관적으로 알 수 있도록 했습니다. 측정날짜(measurementDate)와 개수(count) 필드로 이루어진 객체 타입입니다.
    - articleCreationDate: article 모듈에 의존하지 않고 게시물 생성 일자를 저장하여, 일자별 게시물 수를 세기 위해 추가한 필드입니다.
    
    Related to #13

--- src/statistics/schema/article-statistics.schema.ts
+++ src/statistics/schema/article-statistics.schema.ts
@@ -1,10 +1,36 @@
-import { Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
 import { HydratedDocument } from 'mongoose';
+import {
+  ArticleSnsSchemaType,
+  ArticleSnsType,
+  MeasurementUnitByDate,
+} from '../statistics.type';
 
 export type ArticleStatisticsDocument = HydratedDocument<ArticleStatistics>;
 
-@Schema({ timestamps: true, collection: 'statistics' })
-export class ArticleStatistics {}
+@Schema({ timestamps: true })
+export class ArticleStatistics {
+  @Prop({ required: true, type: String })
+  contentId: string;
+
+  @Prop({ required: true, enum: ArticleSnsSchemaType })
+  snsType: ArticleSnsType;
+
+  @Prop({ required: true, type: [String] })
+  hashtags: string[];
+
+  @Prop({ required: true, type: [{ count: Number, measurementDate: Date }] })
+  viewCountByDate: MeasurementUnitByDate[];
+
+  @Prop({ required: true, type: [{ count: Number, measurementDate: Date }] })
+  likeCountByDate: MeasurementUnitByDate[];
+
+  @Prop({ required: true, type: [{ count: Number, measurementDate: Date }] })
+  shareCountByDate: MeasurementUnitByDate[];
+
+  @Prop({ required: true, type: Date })
+  articleCreationDate: Date;
+}
 
 export const ArticleStatisticsSchema =
   SchemaFactory.createForClass(ArticleStatistics);
