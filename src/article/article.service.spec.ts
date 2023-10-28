8943acaf2e41e2621b3b505a722b24cab0f7c628
Refi Go <64242999+refigo@users.noreply.github.com>
2023-10-29 02:34:24 +0900

    Test : ArticleService.findOneByContentId 기본 로직 테스트 #18

--- src/article/article.service.spec.ts
+++ src/article/article.service.spec.ts
@@ -3,12 +3,52 @@ import { ArticleService } from './article.service';
 import { getModelToken } from '@nestjs/mongoose';
 import { Article } from './schema/article.schema';
 import { Model } from 'mongoose';
+import { NotFoundException } from '@nestjs/common';
 
 describe('ArticleService', () => {
   let articleService: ArticleService;
   let model: Model<Article>;
 
-  const mockArticleService = {};
+  const mockArticle = {
+    _id: '653ca8b5e100a45aa521511e',
+    contentId: '56ef756082cd4ddb9690af97ae5ef362',
+    type: 'instagram',
+    title: 'good title',
+    content: 'great content',
+    hashtags: [
+        'nice'
+    ],
+    viewCount: 100,
+    likeCount: 200,
+    shareCount: 300,
+    createdAt: '2023-10-28T06:22:45.862Z',
+    updatedAt: '2023-10-28T06:22:45.862Z',
+    __v: 0,
+    save: jest.fn().mockImplementation(function() {
+      return Promise.resolve(this);
+    })
+
+  }
+  const mockArticleRes = {
+    contentId: '56ef756082cd4ddb9690af97ae5ef362',
+    type: 'instagram',
+    title: 'good title',
+    content: 'great content',
+    hashtags: [
+        'nice'
+    ],
+    viewCount: 101,
+    likeCount: 200,
+    shareCount: 300,
+  }
+
+  // const mockArticleService = {
+  //   findOneByContentId: jest.fn()
+  // };
+  const mockArticleService = {
+    findOne: jest.fn().mockResolvedValue(mockArticle), // Add this line
+    findOneByContentId: jest.fn()
+  };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
@@ -27,5 +67,19 @@ describe('ArticleService', () => {
 
   it('should be defined', () => {
     expect(articleService).toBeDefined();
+  });
+
+  describe('findOneByContentId', () => {
+
+    it('should find and return a article by contentId', async () => {
+      jest.spyOn(model, 'findOne').mockResolvedValue(mockArticle);
+
+      const result = await articleService.findOneByContentId(mockArticle.contentId);
+
+      expect(model.findOne).toHaveBeenCalledWith({
+        contentId: mockArticle.contentId
+      });
+      expect(result).toEqual(mockArticleRes);
+    })
   });
 });
