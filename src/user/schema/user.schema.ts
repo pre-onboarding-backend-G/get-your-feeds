628e997358d8b453bcd05292fd7c8fee342731f5
Sang Un Lee <sangunlee6@gmail.com>
2023-10-28 18:19:53 +0900

    Feat: 유저 생성 구현#15
    - email, password, service, userHashtag를 받으면 해당 정보를 DB에 저장
    - auth 관련 함수를 구현하기 위한 임시 API
    - 추후에 회원가입 API를 다시 작성할 예정
    
    Related to #15

--- src/user/schema/user.schema.ts
+++ src/user/schema/user.schema.ts
@@ -18,18 +18,22 @@ export class User extends Document {
         enum: ['twitter', 'facebook', 'instagram', 'thread'],
         required: true,
       },
-      accountTag: {
+      userHashtag: {
         type: String,
         required: true,
       },
     },
   ])
-  connectedServices: { service: string; accountTag: string }[];
+  connectedServices: { service: string; userHashtag: string }[];
 }
 
 export const UserSchema = SchemaFactory.createForClass(User);
 
 UserSchema.index(
-  { _id: 1, 'connectedServices.service': 1, 'connectedServices.accountTag': 1 },
+  {
+    _id: 1,
+    'connectedServices.service': 1,
+    'connectedServices.userHashtag': 1,
+  },
   { unique: true },
 );
--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Post()
+  @HttpCode(HttpStatus.CREATED)
+  async postUser(
+    @Body('email') email: string,
+    @Body('password') password: string,
+    @Body('service') service: string,
+    @Body('userHashtag') userHashtag: string,
+  ) {
+    await this.userService.createUser(email, password, service, userHashtag);
+
+    return;
+  }
 }
