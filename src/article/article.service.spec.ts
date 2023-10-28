a4c019052e89a66842ae2d027d3c7fd0203cd9a5
Refi Go <64242999+refigo@users.noreply.github.com>
2023-10-29 01:13:44 +0900

    Feat : ArticleService 유닛테스트용 beforeEach 세팅 #6

--- src/article/article.service.spec.ts
+++ src/article/article.service.spec.ts
@@ -1,18 +1,31 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticleService } from './article.service';
+import { getModelToken } from '@nestjs/mongoose';
+import { Article } from './schema/article.schema';
+import { Model } from 'mongoose';
 
 describe('ArticleService', () => {
-  let service: ArticleService;
+  let articleService: ArticleService;
+  let model: Model<Article>;
+
+  const mockArticleService = {};
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ArticleService],
+      providers: [
+        ArticleService,
+        {
+          provide: getModelToken(Article.name),
+          useValue: mockArticleService,
+        }
+      ],
     }).compile();
 
-    service = module.get<ArticleService>(ArticleService);
+    articleService = module.get<ArticleService>(ArticleService);
+    model = module.get<Model<Article>>(getModelToken(Article.name));
   });
 
   it('should be defined', () => {
-    expect(service).toBeDefined();
+    expect(articleService).toBeDefined();
   });
 });
