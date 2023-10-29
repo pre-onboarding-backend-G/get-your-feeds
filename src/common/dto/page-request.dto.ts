42c818bc41924db5abb9254228d17a9c187660bc
Zamoca <suntail2002@naver.com>
2023-10-29 21:08:34 +0900

    Refactor: pageRequestDto 수정 #6
    - 페이지 정보를 dto에서 보여주게 수정
    Realted to #6

--- src/common/dto/page-request.dto.ts
+++ src/common/dto/page-request.dto.ts
@@ -1,26 +1,42 @@
-import { Transform } from 'class-transformer';
-import { IsOptional, IsInt } from 'class-validator';
+import { Type } from 'class-transformer';
+import { IsOptional, IsInt, IsNumber } from 'class-validator';
 
 export class PageRequestDto {
   @IsInt()
+  @Type(() => Number)
   @IsOptional()
-  @Transform(({ value }) => {
-    const parsedValue = parseInt(value)
-    if (isNaN(parsedValue) || parsedValue < 1) {
-      return 1;
-    }
-    return parsedValue
-  })
+  @IsNumber()
   page?: number;
 
   @IsInt()
   @IsOptional()
-  @Transform(({ value }) => {
-    const parsedValue = parseInt(value)
-    if (isNaN(parsedValue) || parsedValue < 10) {
-      return 10;
-    }
-    return parsedValue
-  })
+  @Type(() => Number)
   perPage?: number;
+
+  constructor(
+    page?: number,
+    perPage?: number,
+  ) {
+    this.page = page;
+    this.perPage = perPage;
+  }
+
+  get skip(): number {
+    return this.page <= 0 ? (this.page = 0) : (this.page - 1) * this.perPage;
+  }
+
+  validatePaginateQuery() {
+    this.validatePage();
+    this.validateTake();
+
+    return this;
+  }
+
+  private validateTake() {
+    this.perPage = this.perPage && this.perPage >= 1 ? this.perPage : 10;
+  }
+
+  private validatePage() {
+    this.page = this.page && this.page >= 1 ? this.page : 1;
+  }
 }
