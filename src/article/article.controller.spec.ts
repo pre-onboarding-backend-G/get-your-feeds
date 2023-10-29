a4edea18a9bf4a1ec6322848ce53aa2fd474e432
Refi Go <64242999+refigo@users.noreply.github.com>
2023-10-29 09:55:05 +0900

    Test : ArticleController Test를 위한 Mock 설정 #6

--- src/article/article.controller.spec.ts
+++ src/article/article.controller.spec.ts
@@ -1,14 +1,24 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticleController } from './article.controller';
 import { ArticleService } from './article.service';
+import { Article } from './schema/article.schema';
+import { getModelToken } from '@nestjs/mongoose';
 
 describe('ArticleController', () => {
   let controller: ArticleController;
 
+  const mockArticleService = {};
+
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ArticleController],
-      providers: [ArticleService],
+      providers: [
+		ArticleService,
+		{
+		  provide: getModelToken(Article.name),
+		  useValue: mockArticleService,
+		}
+	],
     }).compile();
 
     controller = module.get<ArticleController>(ArticleController);
